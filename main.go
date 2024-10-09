34334b2817ca051590df767f77992e7246c1ca8d
newmanai97 <939849705@qq.com>
2024-10-09 10:08:59 +0800

    提交leetcode344

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"suanfa/leetcode/question"
 )
 
 func main() {
-	array := []int{-4, -1, 0, 3, 10}
-	result := question.SortedSquares2(array)
-	for i, j := range result {
-		fmt.Println(strconv.Itoa(i) + ":=" + strconv.Itoa(j))
-	}
+	array := []byte{'H', 'a', 'n', 'n', 'a', 'h'}
+	question.ReverseString(array)
+
+	str := string(array[:])
+	fmt.Println(str)
 }
