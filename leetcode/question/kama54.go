e45506b3fc9b353cd50afa93535d4f0097147330
newmanai97 <939849705@qq.com>
2024-10-24 11:03:59 +0800

    提交kama54

---
+++ leetcode/question/kama54.go
@@ -0,0 +1,19 @@
+package question
+
+import "fmt"
+
+func replaceNumber() {
+	var strs []byte
+
+	fmt.Scanln(&strs)
+
+	for i := 0; i < len(strs); i++ {
+		if strs[i] <= '9' && strs[i] >= '0' {
+			insertElement := []byte{'n', 'u', 'm', 'b', 'e', 'r'}
+			strs = append(strs[:i-1], append(insertElement, strs[i+1:]...)...)
+			i = i + len(insertElement) - 1
+		}
+	}
+
+	fmt.Println(string(strs))
+}
